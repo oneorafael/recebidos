4f7686ad9cd455672fb31396cf7ebc10ff0821b2
Rafael Oliveira <oliveirafael@icloud.com>
2026-06-15 18:40:37 -0300

    Initial Commit

---
+++ Recebidos?/Recebidos_App.swift
@@ -0,0 +1,17 @@
+//
+//  Recebidos_App.swift
+//  Recebidos?
+//
+//  Created by Rafael Oliveira on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Recebidos_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
